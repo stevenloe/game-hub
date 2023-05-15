8d8d795161c6b732a7cc0935595149f773329134
Steven Loe <stevenjloe@protonmail.com>
2023-05-15 17:00:20 -0400

    building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,21 +2,22 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
 const useGames = () => {
     // state var to hold game objects we will retrieve from the api
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
 
-    interface Game {
-      id: number;
-      name: string;
-    }
-    
-    interface FetchGamesResponse {
-      count: number;
-      results: Game[];
-    }
-    
     useEffect(() => {
       const controller = new AbortController();
 
