58f19ec02ae70ba0a42608695e748d0cfc5da934
Steven Loe <stevenjloe@protonmail.com>
2023-05-27 09:55:08 -0400

    display genres

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,16 @@ function App() {
         base: `"nav" "main"`, // mobile
         lg: `"nav nav" "aside main"`, // desktop
       }}
+      templateColumns= {{
+        base: '1fr',
+        lg: '200px 1fr'
+      }}
     >
       <GridItem area="nav" >
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
          <GenreList />
         </GridItem>
       </Show>
